a3ff54a5ead0eee7a28cdd7e2b08ab24f5c91798
Pedro Espinoza <pespinoza@microsoft.com>
2023-12-02 13:44:07 -0600

    Updating sub and tenant, cleaning up

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name   = "ProdResourceGroup"
-    storage_account_name  = "tsprosa"
+    storage_account_name  = "truckstopprodsa"
     container_name        = "terraform-state"
     key                   = "prod.terraform.tfstate"
   }
