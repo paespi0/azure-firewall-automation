44484e1207e1d158ce6a747aa05a30b99abf4835
Pedro Espinoza <pespinoza@microsoft.com>
2023-12-02 14:23:49 -0600

    add new rule and update existing rule

--- terraform/rules.tf
+++ terraform/rules.tf
@@ -7,20 +7,20 @@ resource "azurerm_firewall_network_rule_collection" "example_network_rules" {
 
   rule {
     name = "allow-http"
-    source_addresses = ["10.0.0.0/16"]
+    source_addresses = ["10.0.0.0/24"]
     destination_ports = ["80", "443"]
     destination_addresses = ["*"]
     protocols = ["TCP"]
   }
 
   # Add more rules as needed!
-  # rule {
-  #   name = "allow-ssh"
-  #   source_addresses = ["10.0.0.0/16"]
-  #   destination_ports = ["22"]
-  #   destination_addresses = ["*"]
-  #   protocols = ["TCP"]
-  # }
+  rule {
+    name = "allow-ssh"
+    source_addresses = ["10.0.0.0/16"]
+    destination_ports = ["22"]
+    destination_addresses = ["*"]
+    protocols = ["TCP"]
+  }
 
 }
 
