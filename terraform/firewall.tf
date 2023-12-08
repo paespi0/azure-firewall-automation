c1ff4f9b8b66727e0caae7d7f1fe0df2356e3ea4
Pedro Espinoza <pespinoza@microsoft.com>
2023-12-08 13:36:32 -0600

    rm unused rules.tf, updated wf, update fw.tf to create AZ FW policy

--- terraform/firewall.tf
+++ terraform/firewall.tf
@@ -1,9 +1,17 @@
-resource "azurerm_firewall" "example_firewall" {
-  name                = "exampleFirewall"
+resource "azurerm_public_ip" "example" {
+  name                = "examplePublicIp-${var.environment}"
   location            = var.location
   resource_group_name = var.resource_group_name
-  sku_name            = "AZFW_VNet" # This should be set to the appropriate SKU for your use case.
-  sku_tier            = "Standard"  # Choose between "Standard" and "Premium" based on your needs.
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_firewall" "example_firewall" {
+  name                = "exampleFirewall-${var.environment}"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name            = "AZFW_VNet"
+  sku_tier            = "Standard"
 
   ip_configuration {
     name                 = "configuration"
@@ -12,10 +20,52 @@ resource "azurerm_firewall" "example_firewall" {
   }
 }
 
-resource "azurerm_public_ip" "example" {
-  name                = "examplePublicIp"
-  location            = var.location
+resource "azurerm_firewall_policy" "example_policy" {
+  name                = "exampleFirewallPolicy-${var.environment}"
   resource_group_name = var.resource_group_name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+  location            = var.location
+  // Additional properties and rules can be added here
+}
+
+resource "azurerm_firewall_policy_rule_collection_group" "example_policy_rule" {
+  name               = "exampleFirewallPolicyRule-${var.environment}"
+  firewall_policy_id = azurerm_firewall_policy.example_policy.id
+  priority           = 100
+
+  application_rule_collection {
+    name     = "exampleAppRuleCollection"
+    priority = 100
+    action   = "Allow"
+    rule {
+      name = "exampleRule"
+      source_addresses  = ["*"]
+      destination_fqdns = ["www.example.com"]
+      protocols {
+        port = 80
+        type = "Http"
+      }
+      protocols {
+        port = 443
+        type = "Https"
+      }
+    }
+    // Additional rules as needed
+  }
+  // Network rule collections and NAT rule collections can be added as needed
+    network_rule_collection {
+    name     = "exampleNetworkRuleCollection"
+    priority = 200
+    action   = "Allow"
+
+    rule {
+      name = "exampleNetworkRule"
+      source_addresses = ["*"]
+      destination_addresses = ["*"]
+      destination_ports = ["80", "443"]
+      protocols = ["TCP"]
+    }
+    // Additional network rules as needed
+  }
+
+  // NAT rule collections can be added as needed
 }
